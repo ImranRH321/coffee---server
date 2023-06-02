96be2e7b3e4c27c955fd33c4004b799c64a1d652
ImranRH321 <imranrh247@gmail.com>
2023-06-02 17:05:06 +0600

    simple-card-operation

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,14 +34,65 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const coffeeCollection = client.db("coffeeDb").collection("coffee");
- 
+
     // save store db coffee collection
     app.post("/coffee", async (req, res) => {
       const newCoffee = req.body;
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
     });
-    
+    // save store db coffee collection
+    app.get("/coffee", async (req, res) => {
+      const result = await coffeeCollection.find({}).toArray();
+      console.log("result", result);
+      res.send(result);
+    });
+    // coffee delete
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await coffeeCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    // update route single coffee load
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await coffeeCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    // update coffee
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name:body.name,
+          quantity: body.quantity,
+          supplier: body.supplier,
+          taste: body.taste,
+          category: body.category,
+          details: body.details,
+          photo: body.photo,
+        },
+      };
+      console.log(body);
+      console.log("==========body========");
+      const result = await coffeeCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log("==========result========");
+      console.log("result", result);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
