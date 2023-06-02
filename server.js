360e56f6525c6918e5e7530abcc21764f89c7a8d
ImranRH321 <imranrh247@gmail.com>
2023-06-02 13:06:13 +0600

    database save newCoffee

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ console.log(process.env.db_password);
 
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.5tob0mc.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -35,14 +33,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-    console.log("ok  connection");
+    const coffeeCollection = client.db("coffeeDb").collection("coffee");
+ 
+    // save store db coffee collection
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+    
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
